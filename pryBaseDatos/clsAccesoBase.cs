a9ca2a32f6ab1375c113bcfa6cb7ce9ffe7fabc5
root <mgiraudo@ies21.edu.ar>
2023-10-10 17:16:28 -0300

    Agregar archivos de proyecto.

---
+++ pryBaseDatos/clsAccesoBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+
+namespace pryBaseDatos
+{
+    internal class clsAccesoBase
+    {
+        //Declaración de variable y objetos globales
+        OleDbConnection ConexionBD;
+        OleDbCommand comandarBD;
+        OleDbDataReader BDDataReader;
+
+        DirectoryInfo RutaProyecto = new DirectoryInfo(@"../..");
+
+        String cadenaConexion;
+        public string resultadoDatos;
+
+        public bool estadoConexion; //verdado-conectado y si es falso-desconectado
+
+        //constructor: cuando se crea un objeto de la clase acceso base
+    }
+
+}
---
+++ pryBaseDatos/frmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace pryBaseDatos
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.cboUsers = new System.Windows.Forms.ComboBox();
+            this.btnTraer = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(195, 183);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(48, 13);
+            this.lblUsuario.TabIndex = 8;
+            this.lblUsuario.Text = "Usuarios";
+            // 
+            // cboUsers
+            // 
+            this.cboUsers.FormattingEnabled = true;
+            this.cboUsers.Location = new System.Drawing.Point(293, 180);
+            this.cboUsers.Name = "cboUsers";
+            this.cboUsers.Size = new System.Drawing.Size(121, 21);
+            this.cboUsers.TabIndex = 7;
+            // 
+            // btnTraer
+            // 
+            this.btnTraer.Location = new System.Drawing.Point(104, 106);
+            this.btnTraer.Name = "btnTraer";
+            this.btnTraer.Size = new System.Drawing.Size(484, 29);
+            this.btnTraer.TabIndex = 6;
+            this.btnTraer.Text = "Traer informaciòn de USERS";
+            this.btnTraer.UseVisualStyleBackColor = true;
+            this.btnTraer.Click += new System.EventHandler(this.btnTraer_Click);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(104, 35);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(484, 29);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "Conectar a la Base";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(33, 366);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(10, 13);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "-";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.cboUsers);
+            this.Controls.Add(this.btnTraer);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.ComboBox cboUsers;
+        private System.Windows.Forms.Button btnTraer;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
